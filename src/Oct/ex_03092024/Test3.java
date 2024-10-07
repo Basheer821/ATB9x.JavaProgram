678fcf3727a9cdfc8809c6e7e4348d8cf7233dac
Basheer821 <skbasheer821@gmail.com>
2024-10-08 00:18:37 +0530

    conformation

---
+++ src/Oct/ex_03092024/Test3.java
@@ -0,0 +1,11 @@
+package Oct.ex_03092024;
+
+public class Test3 {
+    public static void main(String[] args) {
+        //Calculation
+        System.out.println("addition"   +  (10+20));
+        System.out.println("subtaaction"   +  (10-5));
+        System.out.println("multlipation"   +   2*5);
+        System.out.println("divison"   +  10/4);
+    }
+}
