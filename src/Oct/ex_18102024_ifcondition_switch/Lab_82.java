63f571cfb1b7e458cff61a7fa9d6f9b71d185bce
Basheer821 <skbasheer821@gmail.com>
2024-10-24 12:16:46 +0530

    conformation all data103

---
+++ src/Oct/ex_18102024_ifcondition_switch/Lab_82.java
@@ -0,0 +1,18 @@
+package Oct.ex_18102024_ifcondition_switch;
+
+public class Lab_82 {
+    public static void main(String[] args) {
+        int a=1;
+        switch (-1){
+            default:
+                System.out.println("Default");
+                break;
+            case -1:
+                System.out.println("10");
+                break;
+            case 9:
+                System.out.println("10");
+                break;
+        }
+    }
+}
---
+++ src/Oct/ex_18102024_ifcondition_switch/Lab_83.java
@@ -0,0 +1,14 @@
+package Oct.ex_18102024_ifcondition_switch;
+
+public class Lab_83 {
+    public static void main(String[] args) {
+        char code='C';
+        switch(code){
+            default -> System.out.println("hellooooo");
+            case 'A' -> System.out.println("asdfg");
+            case 'B' -> System.out.println("qwerty");
+            case 'C' -> System.out.println("zxcvb");
+            case 'D' -> System.out.println("qwertyu");
+        }
+    }
+}
