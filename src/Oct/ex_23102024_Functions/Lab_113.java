97e8ecd8cc9284bf51d27f8d19697dad2f38d1af
Basheer821 <skbasheer821@gmail.com>
2024-11-16 18:18:11 +0530

    conformation all data105

---
+++ src/Oct/ex_23102024_Functions/Lab_113.java
@@ -0,0 +1,19 @@
+package Oct.ex_23102024_Functions;
+
+public class Lab_113 {
+    public static void main(String[] args) {
+        // Functions
+        // Block of code which is we can reuse
+
+
+
+        //1.Built in
+        //2.user Defined.
+
+
+        //Two component of the functions
+        //1. Define /Declare
+        //2.call the Functions
+        
+    }
+}
---
+++ src/Oct/ex_23102024_Functions/Lab_115.java
@@ -0,0 +1,19 @@
+package Oct.ex_23102024_Functions;
+
+public class Lab_115 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void main(int a) {
+
+    }
+
+    public static void main(String args) {
+
+    }
+
+    public static int main() {
+        return 18;
+    }
+}
